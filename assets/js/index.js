564d944dbaad79bed492f6e25b05ba9371313614
Sirius <sirius@sirius-S550CB.(none)>
2015-11-18 15:31:38 +0800

    B:疾病類別雙擊兩次原有疾病名稱列表才會消失
    Please enter the commit message for your changes. Lines starting

--- assets/js/index.js
+++ assets/js/index.js
@@ -137,57 +137,58 @@ $(document).ready(function() {
                 'transform': 'translate(0px,125px)'
             });
 
-        $('.test1').on('mousedown touchstart', function() {
-            if (!act1) {
-                $(this).find('.test1_1').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(0px,125px)'
-                });
-                $('.test3').find(".testbutton").css({
-                    'transform': 'none'
-                });
-                act3 = !act3;
-                $('.test2').find(".testbutton").css({
-                    'transform': 'none'
-                });
-                act2 = !act2;
-            } else
-                $(this).find('.test1_1').css({
-                    'background-color': 'darkGray',
-                    'transform': 'none'
-                });
-            if (!act1)
-                $(this).find('.test1_2').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(-60px,105px)'
-                });
-            else
-                $(this).find('.test1_2').css({
-                    'background-color': 'darkGray',
-                    'transform': 'none'
-                });
-            if (!act1)
-                $(this).find('.test1_3').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(-105px,60px)'
-                });
-            else
-                $(this).find('.test1_3').css({
-                    'background-color': 'darkGray',
-                    'transform': 'none'
-                });
-            if (!act1)
-                $(this).find('.test1_4').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(-125px,0px)'
-                });
-            else
-                $(this).find('.test1_4').css({
-                    'background-color': 'darkGray',
-                    'transform': 'none'
-                });
-            act1 = !act1;
-        }); //下面第二層
+            $('.test1').on('mousedown touchstart', function() {
+                if (!act1) {
+                    $(this).find('.test1_1').css({
+                        'background-color': 'gray',
+                        'transform': 'translate(0px,125px)'
+                    });
+                    $('.test3').find(".testbutton").css({
+                        'transform': 'none'
+                    });
+                    act3 = !act3;
+                    $('.test2').find(".testbutton").css({
+                        'transform': 'none'
+                    });
+                    act2 = !act2;
+                } else
+                    $(this).find('.test1_1').css({
+                        'background-color': 'darkGray',
+                        'transform': 'none'
+                    });
+                if (!act1)
+                    $(this).find('.test1_2').css({
+                        'background-color': 'gray',
+                        'transform': 'translate(-60px,105px)'
+                    });
+                else
+                    $(this).find('.test1_2').css({
+                        'background-color': 'darkGray',
+                        'transform': 'none'
+                    });
+                if (!act1)
+                    $(this).find('.test1_3').css({
+                        'background-color': 'gray',
+                        'transform': 'translate(-105px,60px)'
+                    });
+                else
+                    $(this).find('.test1_3').css({
+                        'background-color': 'darkGray',
+                        'transform': 'none'
+                    });
+                if (!act1)
+                    $(this).find('.test1_4').css({
+                        'background-color': 'gray',
+                        'transform': 'translate(-125px,0px)'
+                    });
+                else
+                    $(this).find('.test1_4').css({
+                        'background-color': 'darkGray',
+                        'transform': 'none'
+                    });
+                act1 = !act1;
+            }); //下面第二層
+
         if (active1 == 1) {
             $('.parent2').find('.test1').css({
                 'background-color': 'dimGray',
@@ -353,6 +354,159 @@ $(document).ready(function() {
         active2 = !active2;
         active3 = !active3;
     });
+
+
+    // name----------------------------------------------------------------------------
+    var d_Id=["疾病1","疾病2","疾病3","疾病4","疾病5","疾病6"];
+/*
+var d_ca_1=["天花,嚴重急性呼吸道症候群,鼠疫,狂犬病,更多",["登革熱","德國麻疹","霍亂","流行性斑疹傷寒","白喉","其他"],["腸病毒感染併發重症","結核病","人類免疫缺乏病毒感染","漢生病","百日咳","其他"],["流感併發重症","肉毒桿菌中毒","庫賈氏病","鉤端螺旋體病","萊姆病","其他"],["新型A型流感","中東呼吸症候群冠狀病毒感染症","黃熱病", "伊波拉病毒感染","拉薩熱","其他"],["棘狀阿米巴","茲卡病毒感染","福氏內格里阿米巴腦膜腦炎","貓抓病","NDM-1腸道菌感染症","更多"]];     
+*/
+
+    var d_category_1_1="天花,嚴重急性呼吸道症候群,鼠疫,狂犬病,更多";
+    var d_category_1_2="登革熱,德國麻疹,霍亂,流行性斑疹傷寒,白喉,其他";
+    var d_category_1_3="腸病毒感染併發重症,結核病,人類免疫缺乏病毒感染,漢生病,百日咳,更多";
+    var d_category_1_4="流感併發重症,肉毒桿菌中毒,庫賈氏病,鉤端螺旋體病,萊姆病,更多";
+    var d_category_1_5="新型A型流感,中東呼吸症候群冠狀病毒感染症,黃熱病,伊波拉病毒感染,拉薩熱,更多";
+    var d_category_1_o="棘狀阿米巴,茲卡病毒感染,福氏內格里阿米巴腦膜腦炎,貓抓病,NDM-1腸道菌感染症,更多";
+
+    var d_category_2_1="登革熱,屈公病,瘧疾,日本腦炎,鼠疫,更多";
+    var d_category_2_2="腸病毒感染併發重症,腸道出血性大腸桿菌感染症,傷寒,副傷寒,沙門氏菌感染症,更多";
+    var d_category_2_3="新型A型流感,水痘併發症,結核病,多重抗藥性結核病,流感,更多";
+    var d_category_2_4="人類免疫缺乏病毒感染,梅毒,淋病,急性病毒性Ｂ型肝炎,急性病毒性Ｃ型肝炎,更多";
+    var d_category_2_5="福氏內格里阿米巴腦膜腦炎,狂犬病,炭疽病,類鼻疽,鉤端螺旋體病,更多";
+
+
+    var d_category_3="登革熱,流感,結核病,B型肝炎,肉毒桿菌中毒,更多";
+
+
+    var d_show=0;
+    var d0=$('.dd');
+    var d1=$('#dd1');
+    var d2=$('#dd2');
+    var d3=$('#dd3');
+    var d4=$('#dd4');
+    var d5=$('#dd5');
+    var d6=$('#dd6');
+    var index=0;
+    var r=30,n=5,a=66;
+    var act=0; 
+    d0.click(function(){
+        var str="";
+        act=!act;
+        if(act==1){
+            $(".testbutton").css({
+                transform: 'none'     
+            });
+            $(".test3").css({
+                transform: 'none'     
+            });
+            $(".test2").css({
+                transform: 'none'     
+            });
+            $(".test1").css({
+                transform: 'none'     
+            });
+
+        }
+
+        if($(this).data("l")=="1"){
+            if($(this).data("c")=="1")
+                var str=d_category_1_1.split(",");
+            if($(this).data("c")=="2")
+                var str=d_category_1_2.split(",");
+            if($(this).data("c")=="3")
+                var str=d_category_1_3.split(",");
+            if($(this).data("c")=="4")
+                var str=d_category_1_4.split(",");
+            if($(this).data("c")=="5")
+                var str=d_category_1_5.split(",");
+            if($(this).data("c")=="o")
+                var str=d_category_1_o.split(",");
+        }
+        if($(this).data("l")=="2"){
+            if($(this).data("c")=="1")
+                var str=d_category_2_1.split(",");
+            if($(this).data("c")=="2")
+                var str=d_category_2_2.split(",");
+            if($(this).data("c")=="3")
+                var str=d_category_2_3.split(",");
+            if($(this).data("c")=="4")
+                var str=d_category_2_4.split(",");
+            if($(this).data("c")=="5")
+                var str=d_category_2_5.split(",");
+            if($(this).data("c")=="o")
+                var str=d_category_2_o.split(",");
+        }
+        if($(this).data("l")=="3"){
+            var str=d_category_3.split(",");
+        }
+        for(var j=0;j<=5;j++)
+            d_Id[j]=str[j];  
+ //   var  test=d_ca_1[$(this).data("c")];
+   // var str=test.split(",");
+//d_Id[0]=d_ca_1[$(this).data("c")];
+//    alert(d_ca_1[$(this).data("c")]);
+//d_Id[1]=str[1]; 
+
+        if(d_show==0){
+     
+            for(var ii=1;ii<=6;ii++){
+                $('#dd'+ii).delay(ii*100).show('100');
+                $('#t'+ii).delay(ii*100).show('100');
+            }
+        d_show=1;
+        }
+        else{        
+            for(var ii=1;ii<=6;ii++){
+                $('#dd'+ii).delay(ii*100).hide('100');
+                $('#t'+ii).delay(ii*100).hide('100');
+            }
+            d_show=0;
+        }
+        showId();
+    })
+
+    for(var i=1;i<=6;i++){
+        $('#dd'+i).click(function(){
+            var tmp=d_Id[0];
+            d_Id[0]=d_Id[$(this).data("l")-1];
+            d_Id[$(this).data("l")-1]=tmp;
+            showId();
+            d1.css({
+            'background-color':'red'
+            });
+        });
+        $('#dd'+i).mouseout(function(){
+            // $(this).animate({
+            //     top:'+=15px',
+            //     right:'+=15px',
+            //     height:'-=30px',
+            //     width:'-=30px',
+            // });
+            $(this).css({
+                'line-height':'50px'
+                ,'background-color':'dimGray'
+            });
+    
+        });    
+        $('#dd'+i).mouseover(function(){
+            // $(this).animate({
+            //     top:'-=15px',
+            //     right:'-=15px',
+            //     height:'+=30px',
+            //     width:'+=30px',
+            // });
+            $(this).css({
+                'line-height':'50px'
+                , 'background-color':'red'
+            });
+        })     
+    }
+
+    function showId(){
+        for(var i=1;i<=6;i++)
+            $('#t'+i).text(d_Id[(i-1)]);
+    }
 
 
 
